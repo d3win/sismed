37e906e6a9f6dd67d228303f364271e1420478e2
d3win <d3win@hotmail.com>
2018-04-27 19:26:28 -0500

    prueba1

--- conexion.php
+++ conexion.php
@@ -6,7 +6,10 @@
 
 try 
 		{
-			$conexion = new PDO('mysql:host=localhost;dbname=sistemamedico', 'root', 'conexion');
+
+				$conexion = new PDO('mysql:host=us-cdbr-iron-east-05.cleardb.net;dbname=heroku_45eeb4aea35a5d0','b42d0b318a4d79','efad9b73'); 
+
+			//$conexion = new PDO('mysql:host=localhost;dbname=sistemamedico', 'root', 'conexion');
 
 
 		} catch (PDOException $e) 	
